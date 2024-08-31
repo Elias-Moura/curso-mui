3cfd03db4f57158c97820461aff05d845e8cd8fe
elias-moura <elias.smourayt@gmail.com>
2024-08-31 19:43:17 -0300

    feat: Adicionando tema claro

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,18 @@
+import { Button } from "@mui/material"
 import { Navigate, Route, Routes } from "react-router-dom"
 
 
 export const AppRoutes = () => {
     return (
     <Routes>
-        <Route path="/home" element={<p>Página Inicial</p>}/>
+        <Route path="/home" element={
+            <Button 
+              variant="contained"
+              color="primary"
+            >
+                Página Inicial
+            </Button>
+            }/>
         <Route path="*" element={<Navigate to="/home" />} />
     </Routes>
     )
