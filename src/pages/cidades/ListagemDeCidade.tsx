edfd70cf5b29782ebcd561b8bdc267bbb7d77f5e
elias-moura <elias.smourayt@gmail.com>
2024-09-02 17:19:03 -0300

    Feat: criando funcionalidade para o campo busca.

--- src/pages/cidades/ListagemDeCidade.tsx
+++ src/pages/cidades/ListagemDeCidade.tsx
@@ -1,5 +1,31 @@
 import { Box } from '@mui/material';
+import { LayoutBase } from '../../shared/Layouts';
+import { ListToolBar } from '../../shared/components';
+import { useSearchParams } from 'react-router-dom';
+import { useMemo } from 'react';
 
 export const ListagemDeCidade: React.FC = () => {
-  return <Box>Teste</Box>;
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const busca = useMemo(() => {
+    return searchParams.get('busca') || '';
+  }, [searchParams]);
+
+  return (
+    <LayoutBase
+      pageTitle='Listagem de cidades'
+      barraDeFerramentas={
+        <ListToolBar
+          textoBotaoNovo='Nova'
+          mostrarInputBusca
+          textoDaBusca={busca}
+          aoMudarTextoDeBusca={(texto) =>
+            setSearchParams({ busca: texto }, { replace: true })
+          }
+        />
+      }
+    >
+      TESTE
+    </LayoutBase>
+  );
 };
